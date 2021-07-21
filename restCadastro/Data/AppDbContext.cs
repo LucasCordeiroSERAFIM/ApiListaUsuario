3bb8943a96796c566e87d5909bad8d6d1faa2d3f
Lucas Cordeiro <lucas.cordeiro@edu.uniso.br>
2021-07-21 13:47:26 -0300

    Pull aplicação api

---
+++ restCadastro/Data/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace restCadastro.Data
+{
+    public class AppDbContext:DbContext 
+    {
+      public  AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Usuario>().Property(p => p.Nome).HasMaxLength(80);
+            modelBuilder.Entity<Usuario>().Property(p => p.Email).HasMaxLength(80);
+            modelBuilder.Entity<Usuario>().Property(p => p.Cargo).HasMaxLength(80);
+
+            modelBuilder.Entity<Usuario>().HasData(
+                new Usuario { Id = 1, Nome = "Lucas Cordeiro", Cargo = "Analista de Sistemas I", Email = "Lucas.cordeiro@totvs.br" },
+                new Usuario { Id = 2, Nome = "Joabe Queiroz", Cargo = "Analista de Infra I", Email = "Joabe.Queiroz@totvs.br" }
+                );
+        }
+    }
+}
---
+++ restCadastro/Migrations/20210721163141_Inicial.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace restCadastro.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(type: "varchar(80)", maxLength: 80, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Cargo = table.Column<string>(type: "varchar(80)", maxLength: 80, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Email = table.Column<string>(type: "varchar(80)", maxLength: 80, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.InsertData(
+                table: "Usuarios",
+                columns: new[] { "Id", "Cargo", "Email", "Nome" },
+                values: new object[] { 1, "Analista de Sistemas I", "Lucas.cordeiro@totvs.br", "Lucas Cordeiro" });
+
+            migrationBuilder.InsertData(
+                table: "Usuarios",
+                columns: new[] { "Id", "Cargo", "Email", "Nome" },
+                values: new object[] { 2, "Analista de Infra I", "Joabe.Queiroz@totvs.br", "Joabe Queiroz" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+        }
+    }
+}
